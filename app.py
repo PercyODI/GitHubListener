a92d41ec876ddf52be0f9c281fc3bb74d71681fc
PercyODI <pearse.2008@gmail.com>
2020-01-23 18:38:20 +0000

    Added basic github endpoint.

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello_world():
     return "Hello, World!"
+
 
 @app.route("/What")
 def what():
     return {
         "What": "Is this?",
         "DoYouUnderstand": True,
-        "HaveANumber": 5.5
+        "HaveANumber": 5.5,
+        "anotherProp": "Yup"
     }
+
+
+@app.route("/github", methods=["POST"])
+def githubRoot():
+    print(request.get_json())
+    return ('', 204)
